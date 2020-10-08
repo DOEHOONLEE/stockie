3df56ca58947643c532c5cd58c1fef8540e503de
David Lee <genesisparadox@gmail.com>
2020-10-08 20:26:19 +0900

    now refreshes every 30 minutes

--- src/stockIndex.js
+++ src/stockIndex.js
@@ -1,5 +1,6 @@
 // FUNCTIONS
 
+// 검색창을 이용해 새로운 종목 로컬 스토리지에 추가하기
 function addNewStock(stockItem) {
     stockInfo = [stockItem, stockCountry.value]
     localStorage.setItem(localStorage.length, JSON.stringify(stockInfo));
@@ -16,8 +17,8 @@ searchButton.addEventListener("click", function(e) {
         alert("코드를 정확히 입력해주세요.");
     }
     let bookmarkedItems = {...localStorage};
-
     const title = JSON.parse(bookmarkedItems[bookmarkedItems.length-1])[0];
     const country = JSON.parse(bookmarkedItems[bookmarkedItems.length-1])[1];
+    console.log(`${title}..//..${country}`)
     stockDisplay(title, country);
 });
